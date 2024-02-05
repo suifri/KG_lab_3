c2df2960621496bd53b31a444ad1c652bc33c801
suifri <illia.khveshchuk.pz.2021@lpnu.ua>
2024-02-05 20:25:08 +0200

    rot_jebal/of/this/laba

--- KG_lab_3/TrimmedBSplineSurface.cpp
+++ KG_lab_3/TrimmedBSplineSurface.cpp
@@ -8,35 +8,35 @@ TrimmedBSplineSurface::TrimmedBSplineSurface()
 void TrimmedBSplineSurface::display()
 {
     GLfloat knots[8] = { 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0 };
-    GLfloat curveKnots[8] = { 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0 };
-    GLfloat curvePt[6][2] = /* clockwise */
-    { {0.25, 0.5}, {0.25, 0.75}, {0.75, 0.75}, {0.75, 0.5}, {0.5, 0.25}, {0.25, 0.5} };
-    GLfloat pwlPt[4][2] = /* clockwise */
-    { {0.75, 0.5}, {0.5, 0.25}, {0.25, 0.5} };
-
-    GLfloat edgePt[5][2] = /* counter clockwise */
-    { {0.0, 0.0}, {1.0, 0.0}, {1.0, 1.0}, {0.0, 1.0},
-     {0.0, 0.0} };
+    GLfloat edgePt[5][2] ={ {0.0, 0.0}, {1.0, 0.0}, {1.0, 1.0}, {0.0, 1.0}, {0.0, 0.0} };
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glPushMatrix();
     glRotatef(330.0, 1., 0., 0.);
     glScalef(0.5, 0.5, 0.5);
 
-    gluBeginSurface(this->nurbs);
+   gluBeginSurface(this->nurbs);
     gluNurbsSurface(this->nurbs, 8, knots, 8, knots,
         4 * 3, 3, &this->controlPoints[0][0][0],
         4, 4, GL_MAP2_VERTEX_3);
 
-    //gluBeginTrim(this->nurbs);
-    //gluPwlCurve(this->nurbs, 5, &edgePt[0][0], 2,
-    //    GLU_MAP1_TRIM_2);
-    //gluEndTrim(this->nurbs);
+    gluBeginTrim(this->nurbs);
+    gluPwlCurve(this->nurbs, 5, &edgePt[0][0], 2,
+        GLU_MAP1_TRIM_2);
+    gluEndTrim(this->nurbs);
 
     gluBeginTrim(this->nurbs);
-    gluNurbsCurve(this->nurbs, 8, curveKnots, 2,
-        &edgePt[0][0], 5, GLU_MAP1_TRIM_2);
+
+    GLfloat ctrlPoints1[4][3] = {
+    {0.2, 0.2, 1.0},
+    {0.5, 0.866, 1.0},
+    {1.0, 0.0, 1.0},
+    {0.2, 0.2, 1.0} 
+    };
+
+    gluNurbsCurve(this->nurbs, 8, knots, 3, &ctrlPoints1[0][0], 4, GLU_MAP1_TRIM_3);
     gluEndTrim(this->nurbs);
+
     gluEndSurface(this->nurbs);
 
     glPopMatrix();
